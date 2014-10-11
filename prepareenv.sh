3f144cf29e5c88d98b589cf42dd9e271d4c277d1
Mason Mei <dongxu.m@gmail.com>
2014-10-11 23:25:35 +0800

    remove sourcelist change

--- prepareenv.sh
+++ prepareenv.sh
@@ -6,19 +6,19 @@ dockerPath=/usr/bin/docker
 if [ ! $docker = $dockerPath ]
 then
 cd /etc/apt/
-sudo cp sources.list sources.list.backup
-sudo cat > sources.list << 'EOF'
-deb http://mirrors.163.com/ubuntu/ trusty main restricted universe multiverse
-deb http://mirrors.163.com/ubuntu/ trusty-security main restricted universe multiverse
-deb http://mirrors.163.com/ubuntu/ trusty-updates main restricted universe multiverse
-deb http://mirrors.163.com/ubuntu/ trusty-proposed main restricted universe multiverse
-deb http://mirrors.163.com/ubuntu/ trusty-backports main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ trusty main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ trusty-security main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ trusty-updates main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ trusty-proposed main restricted universe multiverse
-deb-src http://mirrors.163.com/ubuntu/ trusty-backports main restricted universe multiverse
-EOF
+#sudo cp sources.list sources.list.backup
+#sudo cat > sources.list << 'EOF'
+#deb http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse
+#deb http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse
+#deb http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse
+#deb http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse
+#deb http://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse
+#deb-src http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse
+#deb-src http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse
+#deb-src http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse
+#deb-src http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse
+#deb-src http://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse
+#EOF
 wget -qO- https://get.docker.io/gpg | sudo apt-key add -
 sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 sudo apt-get update -y
@@ -38,5 +38,5 @@ cd ~/docker/source/
 sudo apt-get install debootstrap -y
 git clone https://github.com/docker/docker.git
 cd docker/contrib/
-sudo ./mkimage.sh -t $vendor/ubuntu debootstrap --include=ubuntu-minimal --components=main,universe trusty  http://mirrors.163.com/ubuntu/
+sudo ./mkimage.sh -t $vendor/ubuntu debootstrap --include=ubuntu-minimal --components=main,universe trusty  http://mirrors.aliyun.com/ubuntu/
 fi
