3330d8fe9c106739d9b2e38299659a84e22f82b5
mason <dongxu.m@gmail.com>
2014-10-11 17:41:11 +0800

    Update build.sh

--- images/build/build.sh
+++ images/build/build.sh
@@ -2,7 +2,7 @@
 
 image=igitras/build-base
 chkresult=`docker images |grep $image |awk '{print $1}'`
-if [ $chkresult != $image ]
+if [ ! $chkresult = $image ]
 then
-docker build -t igitras/build-base .
+docker build -t igitras/$image .
 fi
