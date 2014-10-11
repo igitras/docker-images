aafece8683425638f0222c4abe0c1d8bcc5b34ec
mason <dongxu.m@gmail.com>
2014-10-11 12:55:20 +0800

    Update build.sh

--- images/apache/build.sh
+++ images/apache/build.sh
@@ -2,8 +2,20 @@
 
 mkdir src
 cd src
-wget http://mirrors.hust.edu.cn/apache//httpd/httpd-2.4.10.tar.gz
-wget http://mirrors.hust.edu.cn/apache//apr/apr-1.5.1.tar.gz
-wget http://mirrors.hust.edu.cn/apache//apr/apr-util-1.5.4.tar.gz
+if [ ! -f httpd-2.4.10.tar.gz ]
+then
+wget http://mirrors.hust.edu.cn/apache/httpd/httpd-2.4.10.tar.gz
+fi
+
+if [ ! -f apr-1.5.1.tar.gz ]
+then
+wget http://mirrors.hust.edu.cn/apache/apr/apr-1.5.1.tar.gz
+fi
+
+if [ ! -f apr-util-1.5.4.tar.gz ]
+then
+wget http://mirrors.hust.edu.cn/apache/apr/apr-util-1.5.4.tar.gz
+fi
+
 cd ..
 docker build -t igitras/apache24 .
