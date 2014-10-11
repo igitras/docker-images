47e1a2f8c4b8e4396013b1c3577ad5e68f011c11
mason <dongxu.m@gmail.com>
2014-10-11 18:32:48 +0800

    Update prepareenv.sh

--- prepareenv.sh
+++ prepareenv.sh
@@ -15,11 +15,11 @@ deb-src http://mirrors.oschina.net/ubuntu/ trusty-proposed main restricted unive
 deb-src http://mirrors.oschina.net/ubuntu/ trusty-security main restricted universe multiverse
 deb-src http://mirrors.oschina.net/ubuntu/ trusty-updates main restricted universe multiverse
 EOF
-sudo apt-get install build-essential -y
 wget -qO- https://get.docker.io/gpg | sudo apt-key add -
 sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apt-get install build-essential -y
 sudo apt-get install lxc-docker -y
 sudo gpasswd -a ${USER} docker
 sudo service docker restart
@@ -28,10 +28,11 @@ fi
 
 if [ -x docker ]
 then
+vendor=mason
 mkdir ~/docker/source/ -p
 cd ~/docker/source/
 sudo apt-get install debootstrap -y
 git clone https://github.com/docker/docker.git
 cd docker/contrib/
-sudo ./mkimage.sh -t my/ubuntu debootstrap --include=ubuntu-minimal --components=main,universe trusty  http://mirrors.oschina.net/ubuntu/
+sudo ./mkimage.sh -t $vendor/ubuntu debootstrap --include=ubuntu-minimal --components=main,universe trusty  http://mirrors.oschina.net/ubuntu/
 fi
