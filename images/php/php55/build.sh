f262163cc54da0ceb2a73a80ede701db4002b1cd
mason <dongxu.m@gmail.com>
2014-10-11 13:18:44 +0800

    Update build.sh

---
+++ images/php/php55/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+mkdir src
+cd src
+
+if [ ! -f php-5.5.17.tar.gz ]
+then
+http://cn2.php.net/get/php-5.5.17.tar.gz/from/this/mirror
+fi
+
+cat > php.conf << 'EOF'
+<Directory /application/www>
+    Options Indexes FollowSymLinks
+    AllowOverride All
+    Require all granted
+</Directory>
+<FilesMatch "\.ph(p[2-6]?|tml)$" >
+    SetHandler application/x-httpd-php
+</FilesMatch>
+DirectoryIndex index.php
+
+DocumentRoot /application/www/html
+EOF
+
+cat > test.php << 'EOF'
+<?php
+    phpinfo();
+?>
+EOF
+
+cd ..
+docker build -t igitras/php55 .
