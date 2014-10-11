f7e2b7eecd2109b9fba6e81f9c66486f9356897b
Mason Mei <dongxu.m@gmail.com>
2014-10-11 21:36:59 +0800

    Change mirrors to 163

--- prepareenv.sh
+++ prepareenv.sh
@@ -1,19 +1,23 @@
-#!/bin/sh
-if [ ! -x docker ]
+#!/bin/bash
+
+docker=`which docker`
+dockerPath=/usr/bin/docker
+
+if [ ! $docker = $dockerPath ]
 then
 cd /etc/apt/
 sudo cp sources.list sources.list.backup
 sudo cat > sources.list << 'EOF'
-deb http://mirrors.oschina.net/ubuntu/ trusty main restricted universe multiverse
-deb http://mirrors.oschina.net/ubuntu/ trusty-backports main restricted universe multiverse
-deb http://mirrors.oschina.net/ubuntu/ trusty-proposed main restricted universe multiverse
-deb http://mirrors.oschina.net/ubuntu/ trusty-security main restricted universe multiverse
-deb http://mirrors.oschina.net/ubuntu/ trusty-updates main restricted universe multiverse
-deb-src http://mirrors.oschina.net/ubuntu/ trusty main restricted universe multiverse
-deb-src http://mirrors.oschina.net/ubuntu/ trusty-backports main restricted universe multiverse
-deb-src http://mirrors.oschina.net/ubuntu/ trusty-proposed main restricted universe multiverse
-deb-src http://mirrors.oschina.net/ubuntu/ trusty-security main restricted universe multiverse
-deb-src http://mirrors.oschina.net/ubuntu/ trusty-updates main restricted universe multiverse
+deb http://mirrors.163.com/ubuntu/ trusty main restricted universe multiverse
+deb http://mirrors.163.com/ubuntu/ trusty-security main restricted universe multiverse
+deb http://mirrors.163.com/ubuntu/ trusty-updates main restricted universe multiverse
+deb http://mirrors.163.com/ubuntu/ trusty-proposed main restricted universe multiverse
+deb http://mirrors.163.com/ubuntu/ trusty-backports main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ trusty main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ trusty-security main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ trusty-updates main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ trusty-proposed main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ trusty-backports main restricted universe multiverse
 EOF
 wget -qO- https://get.docker.io/gpg | sudo apt-key add -
 sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
@@ -26,7 +30,7 @@ sudo service docker restart
 sudo reboot
 fi
 
-if [ -x docker ]
+if [ $docker = $dockerPath ]
 then
 vendor=mason
 mkdir ~/docker/source/ -p
@@ -34,5 +38,5 @@ cd ~/docker/source/
 sudo apt-get install debootstrap -y
 git clone https://github.com/docker/docker.git
 cd docker/contrib/
-sudo ./mkimage.sh -t $vendor/ubuntu debootstrap --include=ubuntu-minimal --components=main,universe trusty  http://mirrors.oschina.net/ubuntu/
+sudo ./mkimage.sh -t $vendor/ubuntu debootstrap --include=ubuntu-minimal --components=main,universe trusty  http://mirrors.163.com/ubuntu/
 fi
