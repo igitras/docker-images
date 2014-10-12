75b4f3aa18df4b6995df5f953027776b7d353c8f
Mason Mei <dongxu.m@gmail.com>
2014-10-12 13:56:39 +0800

    fix Java6 download error

--- images/java/java6/build.sh
+++ images/java/java6/build.sh
@@ -14,7 +14,7 @@ cd src
 
 if [ ! -f jdk-6u45-linux-x64.tar.gz ]
 then
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn/java/jdk/6u45-b06/jdk-6u45-linux-x64.bin"
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/6u45-b06/jdk-6u45-linux-x64.bin"
 chmod +x jdk-6u45-linux-x64.bin
 ./jdk-6u45-linux-x64.bin
 tar czvf jdk-6u45-linux-x64.tar.gz jdk1.6.0_45/
