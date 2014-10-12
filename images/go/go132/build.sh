4f78ec02c9072f54c78f18af7cb29a66c78a86c6
Mason Mei <dongxu.m@gmail.com>
2014-10-12 13:12:57 +0800

    Fix go download issue

--- images/go/go132/build.sh
+++ images/go/go132/build.sh
@@ -13,7 +13,10 @@ mkdir src
 cd src
 
 VERSION=1.3.2
+if [ ! -f go$VERSION.linux-amd64.tar.gz ]
+then
 wget http://www.golangtc.com/static/go/go$VERSION.linux-amd64.tar.gz
+fi
 
 cd ..
 docker build -t $image .
