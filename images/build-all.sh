cd58f8181da19b803733799f6febfdbb50e90013
Mason Mei <dongxu.m@gmail.com>
2014-10-13 23:30:43 +0800

    Add hudson build to all docker build files

--- images/build-all.sh
+++ images/build-all.sh
@@ -38,22 +38,12 @@ cd java && ./build.sh && cd ..
 echo "build tomcat images base on java images"
 cd tomcat && ./build.sh && cd ..
 
+# Build all hudson images
+echo "build hudson image"
+cd hudson && ./build.sh && cd ..
 
 # Build Mysql images
 cd mysql && ./build.sh && cd ..
-
-# Build all the tomcats base all java images
-#docker build -t igitras/java6-tomcat6 tomcat/java6-tomcat6
-#docker build -t igitras/java6-tomcat7 tomcat/java6-tomcat7
-#docker build -t igitras/java6-tomcat8 tomcat/java6-tomcat8
-
-#docker build -t igitras/java7-tomcat6 tomcat/java7-tomcat6
-#docker build -t igitras/java7-tomcat7 tomcat/java7-tomcat7
-#docker build -t igitras/java7-tomcat8 tomcat/java7-tomcat8
-
-#docker build -t igitras/java8-tomcat6 tomcat/java8-tomcat6
-#docker build -t igitras/java8-tomcat7 tomcat/java8-tomcat7
-#docker build -t igitras/java8-tomcat8 tomcat/java8-tomcat8
 
 
 # Build all the Mongo images
