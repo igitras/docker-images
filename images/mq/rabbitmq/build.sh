e5777afeeed1277feb96b055a6ddc50692a16992
Mason Mei <dongxu.m@gmail.com>
2014-11-14 20:29:15 +0800

    RabbitMQ

---
+++ images/mq/rabbitmq/build.sh
@@ -0,0 +1,26 @@
+http://www.rabbitmq.com/releases/rabbitmq-server/v3.4.1/rabbitmq-server-generic-unix-3.4.1.tar.gz
+
+#!/bin/bash
+
+vendor=igitras
+imageName=rabbitmq
+image=$vendor/$imageName
+chkresult=`docker images |grep $image |awk '{print $1}'`
+if [ -z $chkresult ]
+then
+echo "bulid rabbitmq image"
+
+mkdir src
+cd src
+
+VERSION=3.4.1
+
+if [ ! -f rabbitmq-server-generic-unix-$VERSION.tar.gz ]
+then
+wget http://www.rabbitmq.com/releases/rabbitmq-server/v$VERSION/rabbitmq-server-generic-unix-$VERSION.tar.gz
+fi
+
+cd ..
+echo "build image with name $image"
+docker build -t $image .
+fi
