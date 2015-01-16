6d9cd5ab9cd72a0f7ad192f0c363cbf8380e3db5
Ubuntu <igitras@igitras.igitras.b4.internal.chinacloudapp.cn>
2015-01-16 07:52:58 +0000

    Change the script errors.

--- prepareenv.sh
+++ prepareenv.sh
@@ -3,9 +3,8 @@
 docker=`which docker`
 dockerPath=/usr/bin/docker
 
-if [ ! $docker = $dockerPath ]
-then
-cd /etc/apt/
+if [  "$docker" != "$dockerPath" ]; then
+    cd /etc/apt/
 #sudo cp sources.list sources.list.backup
 #sudo cat > sources.list << 'EOF'
 #deb http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse
@@ -19,24 +18,21 @@ cd /etc/apt/
 #deb-src http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse
 #deb-src http://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse
 #EOF
-wget -qO- https://get.docker.io/gpg | sudo apt-key add -
-sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install build-essential -y
-sudo apt-get install lxc-docker -y
-sudo gpasswd -a ${USER} docker
-sudo service docker restart
-sudo reboot
-fi
-
-if [ $docker = $dockerPath ]
-then
-vendor=igitras
-mkdir ~/docker/source/ -p
-cd ~/docker/source/
-sudo apt-get install debootstrap -y
-git clone https://github.com/docker/docker.git
-cd docker/contrib/
-sudo ./mkimage.sh -t $vendor/ubuntu debootstrap --include=ubuntu-minimal --components=main,universe trusty  http://mirrors.aliyun.com/ubuntu/
+    wget -qO- https://get.docker.io/gpg | sudo apt-key add -
+    sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    sudo apt-get install build-essential -y
+    sudo apt-get install lxc-docker -y
+    sudo gpasswd -a ${USER} docker
+    sudo service docker restart
+    sudo reboot
+else
+    vendor=igitras
+    mkdir ~/docker/source/ -p
+    cd ~/docker/source/
+    sudo apt-get install debootstrap -y
+    git clone https://github.com/docker/docker.git
+    cd docker/contrib/
+    sudo ./mkimage.sh -t $vendor/ubuntu debootstrap --include=ubuntu-minimal --components=main,universe trusty  http://mirrors.aliyun.com/ubuntu/
 fi
